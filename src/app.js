31e81c50b4a62a7b96fb27dc03005fad78b39751
abhi <abhitarra143@gmail.com>
2026-04-19 03:09:39 +0530

    cors changes

--- src/app.js
+++ src/app.js
@@ -23,13 +23,11 @@ const limiter = rateLimit({
 });
 
 
-app.use(limiter);
-app.use(cors({
-  credentials: true
-}));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(compression());
+app.use(limiter);
 
 app.use("/auth", require("./routes/auth.routes"));
 app.use('/api/dogs', dogRoutes);
